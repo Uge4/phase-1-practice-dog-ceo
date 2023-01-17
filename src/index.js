2a9586c9dea1cdf7c436793d4c3370f81cb5ca25
eugene <eugeneholenstein@gmail.com>
2023-01-17 22:17:26 +1100

    completed challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('%c HI', 'color: firebrick')
+function fetchDogs() {
+  // To pass the tests, don't forget to return your fetch!
+  fetch(`https://dog.ceo/api/breeds/image/random/4`)
+  .then(res => res.json())
+  .then(dogs => displayDogs(dogs))
+  
+}
+
+function displayDogs(dogs) {
+  
+    const container = document.querySelector('#dog-image-container')
+
+    const messages = Object.values(dogs["message"]);
+    
+
+    for(let dog in messages) { 
+      
+      // const result = messages[dog];
+      const image = document.createElement('img')
+      image.src  = messages[dog]
+      document.querySelector('#dog-image-container').appendChild(image)
+    
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchDogs();
+});
+
