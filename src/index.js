29dd993a2c63b9d16a27a23fd541d776f85f6ae1
eugene <eugeneholenstein@gmail.com>
2023-01-18 19:32:15 +1100

    completed challenge 2 & 3

--- src/index.js
+++ src/index.js
@@ -1,29 +1,66 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+// challenge 1
+
 function fetchDogs() {
   // To pass the tests, don't forget to return your fetch!
   fetch(`https://dog.ceo/api/breeds/image/random/4`)
   .then(res => res.json())
-  .then(dogs => displayDogs(dogs))
-  
+  .then(dogs => displayDogs(dogs))  
 }
 
 function displayDogs(dogs) {
-  
-    const container = document.querySelector('#dog-image-container')
-
     const messages = Object.values(dogs["message"]);
-    
-
     for(let dog in messages) { 
-      
-      // const result = messages[dog];
       const image = document.createElement('img')
       image.src  = messages[dog]
       document.querySelector('#dog-image-container').appendChild(image)
-    
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+
+// challenge 2 & 3
+
+function fetchBreeds(){
+  fetch(`https://dog.ceo/api/breeds/list/all`)
+  .then(res => res.json())
+  .then(breeds => addBreeds(breeds))
+}
+
+function addBreeds(breeds){
+
+  const breedList = Object.keys(breeds["message"]);
+
+  const ul = document.getElementById('dog-breeds')
+  const listItems = ul.getElementsByTagName("li") // returns html collection of li's
+
+  for(let dog in breedList) { 
+    
+    // create and render li's with breed info
+    let li = document.createElement('li')
+    li.textContent = breedList[dog]
+    // console.log(li);
+    li.style.color = 'black'
+    document.querySelector('#dog-breeds').appendChild(li)
+   
+    // add interactivity to li's
+    li.addEventListener('click', function(){
+      if(li.style.color === 'black'){
+          li.style.color = 'red'
+        } else 
+        {li.style.color = 'black'
+        }
+  })
+
+  }
+}
+
+
   fetchDogs();
+  fetchBreeds();
+
+
+
 });
 
